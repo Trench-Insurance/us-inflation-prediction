61e2469298e5e07150f66830871e9abc83f5dffe
stefanliemawan <stefanliemawan@gmail.com>
2022-11-05 17:49:11 +0000

    added autoreg and fixed linearreg

--- functions.py
+++ functions.py
@@ -1,19 +1,55 @@
-import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
-
-def linear_regression(x,y):
-    model = LinearRegression()
-    model.fit(x, y)
-    y_pred = model.predict(x)
-
-    rmse =  mean_squared_error(y, y_pred, squared=True)
-    print(rmse)
-    # 0.43679908465250444
-
+import tensorflow as tf
+from statsmodels.tsa.ar_model import AutoReg
 
 def plot_index_inflation(df):
     df.plot("Month",["Index","Inflation"])
     plt.show()
     plt.savefig("fig")
+
+
+def linear_regression(x_train, y_train, x_test, y_test):
+    model = LinearRegression()
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+
+    rmse =  mean_squared_error(y_test, y_pred, squared=True)
+    print(f"linear_regression rmse: {rmse}")
+
+    # linear_regression rmse: 7.164442386810773e-05
+
+
+def auto_regression(y_train, y_test):
+    model = AutoReg(y_train, lags=10)
+    model_fit = model.fit()
+
+    y_pred = model_fit.predict(len(y_train), len(y_train) + len(y_test) - 1)
+
+    rmse =  mean_squared_error(y_test, y_pred, squared=True)
+    print(f"auto_regression rmse: {rmse}")
+    
+    # auto_regression rmse: 0.0003492280629135441
+
+
+def nn_dense():
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(units=64, activation='relu'),
+        tf.keras.layers.Dense(units=64, activation='relu'),
+        tf.keras.layers.Dense(units=1)
+    ])
+    model.compile(loss=tf.keras.losses.MeanSquaredError(),
+            optimizer=tf.keras.optimizers.Adam(),
+            metrics=[tf.keras.metrics.RootMeanSquaredError(name="rmse")])
+
+    return model
+
+    # Epoch 50/50
+    # 1031/1031 [==============================] - 1s 1ms/step - loss: 4.3914e-04 - rmse: 0.0210
+
+
+def train(model, x_train, y_train, epochs=50, batch_size=32):
+    model.build(x_train.shape)
+    print(model.summary())
+    model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size)
--- inflation.py
+++ inflation.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import normalize
 import functions as functions
 
 LABEL_WIDTH = 1
@@ -9,11 +10,21 @@ WINDOW_SIZE = 2
 
 df = pd.read_csv("dataset/cpi-us_zip/data/cpiai_csv.csv")
 df["Month"] = df.index
-df.dropna(inplace=True)
+# df.dropna(inplace=True)
+
+df["Index"] = normalize(df["Index"].values.reshape(-1,1), axis=0)
 
 print(df.head())
 
 x = df["Month"].values.reshape(-1,1)
-y = df["Inflation"].values
+y = df["Index"].values
 
-functions.linear_regression(x,y)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.15, random_state=42)
+
+print(x_train.shape, x_test.shape)
+
+functions.linear_regression(x_train, y_train, x_test, y_test)
+functions.auto_regression(y_train, y_test)
+
+# nn_dense_model = functions.nn_dense()
+# functions.train(nn_dense_model ,x_train, y_train, epochs=50, batch_size=1)
