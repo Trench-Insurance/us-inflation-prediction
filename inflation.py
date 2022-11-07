dfa978235390dd1ac9a598f04319e9ee442de843
stefanliemawan <stefanliemawan@gmail.com>
2022-11-07 18:32:25 +0000

    cleanup

--- inflation.py
+++ inflation.py
@@ -1,18 +1,17 @@
 import pandas as pd
 import numpy as np
-import tensorflow as tf
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import normalize, StandardScaler
-import functions as functions
+from sklearn.preprocessing import StandardScaler
 
-LABEL_WIDTH = 1
-WINDOW_SIZE = 2
+from sklearn.metrics import mean_squared_error
+import tensorflow as tf
+from statsmodels.tsa.ar_model import AutoReg
+
+PREDICTION_WIDTH = 24
 
 # df = pd.read_csv("dataset/CPIAUCSL.csv")
 df = pd.read_csv("dataset/CPIAUCNS.csv")
-# df.dropna(inplace=True)
 
-# df["Index"] = normalize(df["Index"].values.reshape(-1,1), axis=0)
 scaler = StandardScaler()
 
 df["CPIAUCNS"] = scaler.fit_transform(df["CPIAUCNS"].values.reshape(-1,1))
@@ -26,16 +25,23 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, shuffle
 
 print(x_train.shape, x_test.shape)
 
-# functions.linear_regression(x_train, y_train, x_test, y_test)
-auto_reg_model = functions.auto_regression(y_train, y_test)
+model = AutoReg(y_train, lags=10)
+model.fit()
+
+y_pred = model.predict(len(y_train), len(y_train) + len(y_test) - 1)
+
+rmse =  mean_squared_error(y_test, y_pred, squared=True)
+print(f"auto_regression rmse: {rmse}")
+
+# auto_regression rmse: 0.06099068252540488
+
+x = model.predict(
+    len(y_train), len(y_train) + PREDICTION_WIDTH - 1
+)
+x = np.array(x)
+x = scaler.inverse_transform(x.reshape(-1,1))
+
+print(x)
 
 
 
-
-# y_pred = scaler.inverse_transform(y_pred.reshape(-1,1))
-# y_test = scaler.inverse_transform(y_test.reshape(-1,1))
-
-# print(list(zip(y_pred, y_test)))
-
-# nn_dense_model = functions.nn_dense()
-# functions.train(nn_dense_model ,x_train, y_train, epochs=50, batch_size=1)
