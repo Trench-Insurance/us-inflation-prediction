cd3fafc2c9bbaec6bc26706eac3adec53465ee0d
stefanliemawan <stefanliemawan@gmail.com>
2022-11-12 22:13:15 +0000

    added flask backend

--- inflation.py
+++ inflation.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-
+import pickle
 from sklearn.metrics import mean_squared_error
 import tensorflow as tf
 from statsmodels.tsa.ar_model import AutoReg
@@ -26,22 +26,28 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, shuffle
 print(x_train.shape, x_test.shape)
 
 model = AutoReg(y_train, lags=10)
-model.fit()
+fitted_model = model.fit()
 
-y_pred = model.predict(len(y_train), len(y_train) + len(y_test) - 1)
+with open("auto_regression_inflation.pickle", "wb") as f:
+    pickle.dump(fitted_model, f)
+
+with open("auto_regression_inflation.pickle", "rb") as f:
+    m = pickle.load(f)
+
+y_pred = m.predict(len(y_train), len(y_train) + len(y_test) - 1)
+# auto_regression rmse: 0.06099068252540488
+
 
 rmse =  mean_squared_error(y_test, y_pred, squared=True)
 print(f"auto_regression rmse: {rmse}")
 
-# auto_regression rmse: 0.06099068252540488
+# x = model.predict(
+#     len(y_train), len(y_train) + PREDICTION_WIDTH - 1
+# )
+# x = np.array(x)
+# x = scaler.inverse_transform(x.reshape(-1,1))
 
-x = model.predict(
-    len(y_train), len(y_train) + PREDICTION_WIDTH - 1
-)
-x = np.array(x)
-x = scaler.inverse_transform(x.reshape(-1,1))
-
-print(x)
+# print(x)
 
 
 
